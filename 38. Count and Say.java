0eb8bdb895f8630c20363a82ac39e0cfaafeaf41
Kevin Diu <kevindiujp@gmail.com>
2022-01-23 21:23:11 +0900

    Update 38. Count and Say.java

--- 38. Count and Say.java
+++ 38. Count and Say.java
@@ -1,33 +1,30 @@
 // https://leetcode.com/problems/count-and-say/submissions/
 class Solution {
     public String countAndSay(int n) {
-        return dp(n);
-    }
-    
-    private String dp(int n) {
         if (n == 1) {
             return "1";
         }
         
-        String s = dp(n-1);
+        String s = countAndSay(n-1);
         StringBuilder result = new StringBuilder();
         
-        int cn = s.charAt(0) - '0';
+        int cn = s.charAt(0);
         int cnt = 1;
         for (int i = 1; i < s.length(); i++) {
-            int v = s.charAt(i) - '0';
+            int v = s.charAt(i);
             if (v == cn) {
                 cnt++;
             } else {
                 result.append(cnt);
-                result.append(cn);
+                result.append(cn - '0');
                 
                 cn = v;
                 cnt = 1;
             }
         }
         result.append(cnt);
-        result.append(cn);
+        result.append(cn - '0');
+        
         return result.toString();
     }
 }
